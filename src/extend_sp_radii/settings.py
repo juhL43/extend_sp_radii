7347e54e4bb40f0deff62c35e0025de3333a8f25
juhL43 <jugeb@sas.upenn.edu>
2018-01-12 12:03:14 -0500

    bla

--- src/extend_sp_radii/settings.py
+++ src/extend_sp_radii/settings.py
@@ -57,6 +57,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'extend_sp_radii.urls'
@@ -138,4 +141,23 @@ if DEBUG:
 	STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
 	MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
 	STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), "static", "static"),)
+
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
 
